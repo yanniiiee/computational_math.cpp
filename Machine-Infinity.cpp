94d82298fe6dfc231ba2f5a11e8204bcdb3d5418
yanniiiee <nazarova.ayana25@gmail.com>
2024-09-13 13:20:15 +0300

    Update Machine-Infinity.cpp

--- Machine-Infinity.cpp
+++ Machine-Infinity.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+int main() {
+    float machine_infinity = numeric_limits<float>;
+    cout<<machine_infinity<<endl;
+    return 0;
+}
+
 
